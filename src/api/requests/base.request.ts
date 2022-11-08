6e461e573e20dafa2bd821a72d52a0687115fe6d
Dwork <andrii.svynarov@gmail.com>
2022-11-08 20:50:57 +0200

    [fix] allow error statuses

--- src/api/requests/base.request.ts
+++ src/api/requests/base.request.ts
@@ -6,6 +6,7 @@ import * as tokens from "../../data/tokens.json";
 export class BaseRequest implements AxiosRequestConfig {
     baseUrl: string;
     headers: AxiosRequestHeaders;
+    validateStatus: any;
 
     constructor() {
         this.baseUrl = config.baseUrl;
@@ -14,5 +15,6 @@ export class BaseRequest implements AxiosRequestConfig {
         };
 
         this.headers.Authorization = 'Bearer ' + tokens[config.env][config.mode].token;
+        this.validateStatus = false;
     }
 }
