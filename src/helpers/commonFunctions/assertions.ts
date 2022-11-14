68e4c8b13625757b7488868f7d0b778fb852470a
Dwork <andrii.svynarov@gmail.com>
2022-11-15 00:08:36 +0200

    added annotations for custom functions

--- src/helpers/commonFunctions/assertions.ts
+++ src/helpers/commonFunctions/assertions.ts
@@ -17,16 +17,33 @@ interface Types {
     function: "function"
 }
 
+/**
+ * Check if data has a format of "yyyy-mm-dd hh:mm:ss"
+ * @param date - string
+ * @returns - boolean
+ */
 export function lDateValidation(date: string) {
     const lDateRegex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/
     return lDateRegex.test(date);
 }
 
+/**
+ * Check if data has a format of "yyyy-mm-dd"
+ * @param date - string
+ * @returns - boolean
+ */
 export function sDateValidation(date: string) {
     const sDateRegex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/
     return sDateRegex.test(date);
 }
 
+/**
+ * Check if date format is valid for parameters that could be "null"
+ * @param date - string
+ * @param format - use "long" for "yyyy-mm-dd hh:mm:ss" format or "short" for "yyyy-mm-dd" format
+ *
+ * @return - true | AssertionError
+ */
 export function checkNullableDate(date: string, format: "long"|"short"|never) {
     let passed: boolean;
     const longDate = 'yyyy-mm-dd hh:mm:ss';
