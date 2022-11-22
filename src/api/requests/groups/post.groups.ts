b5a22129dcd2f8a4bb8a95b620c4963a7197a142
Dwork <andrii.svynarov@gmail.com>
2022-11-22 18:26:57 +0200

    POST /api/groups

---
+++ src/api/requests/groups/post.groups.ts
@@ -0,0 +1,36 @@
+import {BaseRequest} from "../base.request";
+
+interface Group {
+    name?: string | number;
+    description?: string;
+    default?: boolean;
+}
+
+export class StoreGroup extends BaseRequest {
+    url: string;
+    method: string;
+    data: Group;
+
+    constructor() {
+        super();
+        this.url = this.baseUrl + "/api/groups";
+        this.method = "POST";
+
+        this.data = {};
+    }
+
+    name(name: string | number) {
+        this.data.name = name;
+        return this;
+    }
+
+    description(desc: string) {
+        this.data.description = desc;
+        return this;
+    }
+
+    default(def: boolean) {
+        this.data.default = def;
+        return this;
+    }
+}
