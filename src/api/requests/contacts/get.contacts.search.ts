585aca3ec3f786b41c551e973cb8345e5710df56
Dworkins <andrii.svynarov@gmail.com>
2022-12-12 22:17:45 +0200

    GET /api/contacts/search

---
+++ src/api/requests/contacts/get.contacts.search.ts
@@ -0,0 +1,63 @@
+import {BaseRequest} from "../base.request";
+
+interface SearchContactsRequest {
+    "page[offset]"?: number | null;
+    "page[limit]"?: number | null;
+    sort?: string | null;
+    way?: string | null;
+    "search[field]"?: string | number;
+    "search[operation]"?: ">" | "<" | ">=" | "<=" | "=" | "like" | any;
+    "search[value]"?: lDate | any;
+}
+
+type lDate = string;
+
+
+export class SearchContacts extends BaseRequest {
+    url: string;
+    method: string;
+    params: SearchContactsRequest;
+
+    constructor() {
+        super();
+        this.url = this.baseUrl + "/api/contacts/search";
+        this.method = "GET";
+
+        this.params = {};
+    }
+
+    pageOffset(offset: number) {
+        this.params["page[offset]"] = offset;
+        return this;
+    }
+
+    pageLimit(limit: number) {
+        this.params["page[limit]"] = limit;
+        return this;
+    }
+
+    sort(field: string) {
+        this.params.sort = field;
+        return this;
+    }
+
+    way(way: string) {
+        this.params.way = way;
+        return this;
+    }
+
+    searchField(field: string | number) {
+        this.params["search[field]"] = field;
+        return this;
+    }
+
+    searchOperation(operation: string | any) {
+        this.params["search[operation]"] = operation;
+        return this;
+    }
+
+    searchValue(value: lDate | any) {
+        this.params["search[value]"] = value;
+        return this;
+    }
+}
