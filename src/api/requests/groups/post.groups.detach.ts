df26c3ebb383f8c36b25ec74cc711d1abf15174b
dworkin <andrii.svynarov@gmail.com>
2022-12-13 18:57:11 +0200

    POST /api/groups/detach

---
+++ src/api/requests/groups/post.groups.detach.ts
@@ -0,0 +1,31 @@
+import {BaseRequest} from "../base.request";
+import {Method} from "axios";
+
+interface Attachments {
+    groups?: Array<number>;
+    contacts?: Array<number>;
+}
+
+export class PostGroupsDetach extends BaseRequest {
+    url: string;
+    method: Method;
+    data: Attachments;
+
+    constructor() {
+        super();
+        this.url = this.baseUrl + "/api/groups/detach";
+        this.method = "POST";
+
+        this.data = {};
+    }
+
+    groups(groups: Array<number>) {
+        this.data.groups = groups;
+        return this;
+    }
+
+    contacts(contacts: Array<number>) {
+        this.data.contacts = contacts;
+        return this;
+    }
+}
